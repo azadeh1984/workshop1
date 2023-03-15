61a90223e3c00a35cc1b3edebb676d32c2a9f9b8
azadeh1984 <azadeh_n1984@yahoo.com>
2023-03-15 12:09:50 +0100

    fihxed bugs

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
+import  ReactDOM  from "react-dom";
+import "bootstrap/dist/css/bootstrap.css";
 
 const greetingMessage = <div> Hello React I am Azadeh</div>
+//const root = ReactDOM.creatRoot(document.getElementById(`root`));
+
+ReactDOM.render(greetingMessage, document.getElementById(`root`));
